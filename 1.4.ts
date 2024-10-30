07bc8974262a290deb8aaca2736c0f9495f51c69
nafis200 <nafisahamed14@gmail.com>
2024-10-30 21:38:24 +0600

    first commit

---
+++ 1.4.ts
@@ -0,0 +1,15 @@
+
+let firstname : string = 'nafis'
+
+let isAdmin : boolean = true 
+
+let x : undefined = undefined
+
+let d : number = 120
+
+let friends : string[] = ["nafis","ahamed"]
+
+let roll : number[] = [1,2,3,4,5,6]
+
+// tupple
+let Coordinates : [number,number] = [1,5]
---
+++ 1.5.ts
@@ -0,0 +1,14 @@
+
+
+const user : {
+    firstName: string;
+    lastName: string
+} = {
+    firstName:'nafis',
+    lastName:'ahamed'
+}
+
+console.log(user);
+
+
+
